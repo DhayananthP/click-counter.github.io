eda741a82e4853c449e36a743bdf2740964dfd55
DhayananthP <62584108+DhayananthP@users.noreply.github.com>
2020-10-27 14:00:35 +0530

    Update scripts.js

--- scricpts/scripts.js
+++ scricpts/scripts.js
@@ -1,22 +1,10 @@
-const image = document.querySelector('img');
-const heading = document.querySelector('h1');
-
-image.onclick = function() {
-	image.src = 'images/elephant2.jpg';
-}
-
-const button = document.querySelector('button');
-
-button.onclick = function() {
-	let name = localStorage.getItem('name');
-	if(!name){
-		
-		const promtName =prompt('Enter your name');
-		if(promtName){
-		localStorage.setItem('name',name) ;}
-		
-	}
-	heading.textContent = 'welcome ' +localStorage.getItem('name');
-		
-}
+let i = 0;
+const heading = document.getElementById("heading");
+heading.addEventListener('click' , function() {
+  i++;
+  heading.textContent = i;
+})
+ function reset() {
+    heading.textContent = i = 0;
+};
 	
